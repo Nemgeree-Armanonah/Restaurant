93a961468183fa3494007f2c3ec2a0e71fa2cfec
Ahmadzai Walizadah <nemgeree.armanonah786@gmail.com>
2023-12-20 22:19:51 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <div class="container bg-dark">
+        <h2 class="text-white">Welcome to Our Site</h2>
+    </div>
+
+
+
+
+
+
+@endsection
