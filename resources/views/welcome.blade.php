cb3bbf075611453d7e6e5065bbcc89aa4f1ffb95
Ahmadzai Walizadah <nemgeree.armanonah786@gmail.com>
2023-12-22 18:45:49 +0100

    Renamed images and added speical offer section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-
-
 @section('content')
 
         {{-- Pizza Slider --}}
@@ -24,6 +22,11 @@
                     <img src="/images/Pepperoni.jpg" class="d-block w-100 img-fluid" alt="Pepperoni Pizza">
                   </div>
                 </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/Neapolitan.jpg" class="d-block w-100 img-fluid" alt="Neapolitan Pizza">
+                  </div>
+                </div>
                 <!-- Arrow Buttons -->
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -34,7 +37,220 @@
                   <span class="visually-hidden">Next</span>
                 </button>
               </div>
-            </div> 	<!-- end of container -->
+            </div>
+
+            {{-- Top Sell Pizzas --}}
+
+            <div class="container bg-secondary-color p-4">
+                <h4 class="display-6 text-center">Fequently orderd Pizzas</h4>
+
+                <div class="row m-auto">
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="/images/MeatLover.jpg" class="card-img-top" alt="Neapolitan Pizza">
+                            <div class="card-body">
+                              <h5 class="card-title">Meat Lover Pizza</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="/images/Hawaiian.jpg" class="card-img-top" alt="Hawaiian Pizza">
+                            <div class="card-body">
+                              <h5 class="card-title">Hawaiian Pizza</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="/images/Pepperoni.jpg" class="card-img-top" alt="Pepperoni Pizza">
+                            <div class="card-body">
+                              <h5 class="card-title">Pepperoni Pizza</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="/images/BBQ-chicken.jpg" class="card-img-top" alt="Margarita">
+                            <div class="card-body">
+                              <h5 class="card-title">Margarita Pizza</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+    {{-- Opening Ours  --}}
+    <div class="container-fluid bg-light-subtle" id="openningHours">
+        <h5 class="display-5 text-center">Openning Hours</h5>
+        <p class="lead text-center">We are here to serve you. Make an oder we will take care of it.</p>
+        <div class="row">
+            <div class="col-md-8 m-auto">
+            <table class="table table-borderless text-center p-5 mt-3 shadow-sm bg-body-tertiary bordered rounded-3">
+                <thead>
+                    <th>Day</th>
+                    <th>Pick Up</th>
+                    <th>Delivery</th>
+                </thead>
+                <tr>
+                    <td>Monday</td>
+                    <td>11:00 - 22:00</td>
+                    <td>11:30 - 22:30</td>
+                </tr>
+                <tr>
+                    <td>Tuesday</td>
+                    <td>11:00 - 22:00</td>
+                    <td>11:30 - 22:30</td>
+                </tr>
+                <tr>
+                    <td>Wednesday</td>
+                    <td>11:00 - 22:00</td>
+                    <td>11:30 - 22:30</td>
+                </tr>
+                <tr>
+                    <td>Thursday</td>
+                    <td>11:00 - 22:00</td>
+                    <td>11:30 - 22:30</td>
+                </tr>
+                <tr>
+                    <td>Friday</td>
+                    <td>11:00 - 22:00</td>
+                    <td>11:30 - 22:30</td>
+                </tr>
+                <tr>
+                    <td>Saturday</td>
+                    <td>11:00 - 22:00</td>
+                    <td>11:30 - 22:30</td>
+                </tr>
+                <tfoot>
+                    <td>Don't hesitate to ask Qeustions! </td>
+                    <td><a href="" class="btn btn-outline-danger"><span class="bi bi-phone">(+49) 1632435061</span></a></td>
+                    <td><a href="" class="btn btn-outline-danger"><span class="bi bi-email">Afghan Palaw</span></a></td>
+                </tfoot>
+            </table>
+            </div>
+
+        </div>
+    </div>
+
+  {{-- Spcial Offers --}}
+  <div class="container mb-5">
+    <h4 class="display-5 text-center p-4">Special Offers</h4>
+    <p class="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id tempora esse minus sequi blanditiis at magni cupiditate, consequatur ipsam facilis impedit quasi aliquam necessitatibus ullam architecto nisi voluptas fuga rerum?</p>
+
+    <div class="row">
+      <div class="col-md-3">
+        <div class="card special-offer" style="width: 18rem;">
+          <img src="/images/Pepperoni.jpg" class="card-img-top" alt="Pepperoni Pizza">
+          <div class="card-body">
+            <h5 class="card-title text-center">Pepperoni Pizza</h5>
+            <h5 class="text-muted text-center">Price</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card special-offer" style="width: 18rem;">
+          <img src="/images/Pepperoni.jpg" class="card-img-top" alt="Pepperoni Pizza">
+          <div class="card-body">
+            <h5 class="card-title text-center">Pepperoni Pizza</h5>
+            <h5 class="text-muted text-center">Price</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card special-offer" style="width: 18rem;">
+          <img src="/images/Pepperoni.jpg" class="card-img-top" alt="Pepperoni Pizza">
+          <div class="card-body">
+            <h5 class="card-title text-center">Pepperoni Pizza</h5>
+            <h5 class="text-muted text-center">Price</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="card special-offer" style="width: 18rem;">
+          <img src="/images/Pepperoni.jpg" class="card-img-top" alt="Pepperoni Pizza">
+          <div class="card-body">
+            <h5 class="card-title text-center">Pepperoni Pizza</h5>
+             <h5 class="text-muted text-center">Price</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+    {{-- Footer --}}
+	<div class="container-fluid p-5  bg-black text-light" id="footer">
+		<div class="row mt-5">
+			<!-- Address Section -->
+			<div class="col-md-3 text-center">
+				<h4 class="text-center">Our Adresses:</h4>
+			  <dl>
+				<dt>First Adress</dt>
+				<dd>Am Gorbach 5, 59394 Nordkirchen</dd>
+
+				<dt>Second Adress</dt>
+				<dd>Am Gorbach 5, 59394 Nordkirchen</dd>
+
+				<dt>Third Adress</dt>
+				<dd>Am Gorbach 5, 59394 Nordkirchen</dd>
+
+				<dt>Forth Adress</dt>
+				<dd>Am Gorbach 5, 59394 Nordkirchen</dd>
+			  </dl>
+			</div>
+
+			<!-- Logo and Brand -->
+			<div class="col-md-6 text-center align-items-center footer">
+				<img src="./images/Afghan.png" alt="Logo at the Footer" class="img-fluid">
+			</div>
+
+			<!-- Social contacts-->
+			<div class="col-md-3 social-contact">
+				<h4 class="text-center">Get in Touch</h4>
+				 <ul class="text-center list-unstyled">
+					<li><i class="bi bi-facebook"></i></li>
+					<li><i class="bi bi-instagram"></i></li>
+					<li><i class="bi bi-youtube"></i></li>
+					<li><i class="bi bi-whatsapp"></i></li>
+				 </ul>
+			</div>
+		</div>
+	</div>
+
+
 
 
 @endsection
