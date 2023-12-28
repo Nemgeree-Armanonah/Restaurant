a626c4c5a39cbd5b7d9b01483071d12095f7d2d3
Ahmadzai Walizadah <nemgeree.armanonah786@gmail.com>
2023-12-28 15:12:54 +0100

    Added Pizza table

---
+++ database/migrations/2023_12_28_135906_create_pizzas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id('pizza_id');
+            $table->string('pizza_name');
+            $table->json('pizza_size');
+            $table->integer('pizza_amount');
+            $table->double('pizza_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};
